3cb8bbee2c2d9bef38bcc4a4f28e52c4704f1e24
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-08-31 21:18:01 +0200

    chore: update dependencies for React and ESLint
    - Upgraded @types/react to version 19.1.12
    - Upgraded @types/react-dom to version 19.1.9
    - Updated ESLint to version 9.34.0
    - Updated eslint-plugin-react to version 7.37.5
    - Updated eslint-plugin-react-hooks to version 5.2.0
    - Updated eslint-plugin-react-refresh to version 0.4.20

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,14 +9,11 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
+  settings: { react: { version: "19.0" } },
   plugins: ["react-refresh"],
   rules: {
     "react/jsx-no-target-blank": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true }
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "react/prop-types": "off"
   }
 };
